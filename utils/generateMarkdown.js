33fcb7a3a7ce987b64b4f988ccd5aca7d6825f5e
Amr Salama <asalama@CSG-LOXO.local>
2024-09-20 23:19:45 -0400

    Updated fucntion to retun license section of README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,11 +8,33 @@ function renderLicenseBadge(license) {
 }
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'GPLv3':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'BSD 3-Clause':
+      return 'https://opensource.org/licenses/BSD-3-Clause';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+  return `## License
+This project is licensed under the [${license}](${renderLicenseLink(license)}) license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
