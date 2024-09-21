67bd754671d35a5784dbd3dde4263da660726663
Amr Salama <asalama@CSG-LOXO.local>
2024-09-20 22:37:45 -0400

    Added fileand deinquirer modules

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 import fs from 'fs';
 import inquirer from 'inquirer';
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
+const questions = []
+// TODO: Create a function to write README file 
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
